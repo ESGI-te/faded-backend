5527b6324d1f2a4547ed8ae5a5c2f1f762389f27
tlugat <teo.lugat@ausha.co>
2023-10-15 14:04:43 +0200

    ✨ User groups

---
+++ src/Controller/UserInfoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Security\Core\User\UserInterface;
+#[AsController]
+class UserInfoController extends AbstractController
+{
+    public function __invoke(): UserInterface
+    {
+        $user = $this->getUser();
+
+        return $user;
+    }
+}
