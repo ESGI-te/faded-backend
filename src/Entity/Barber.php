289344a8345980a6a2d95691d6ad2dba619b1590
kachakib <karmimchakib@gmail.com>
2023-10-26 20:21:10 +0200

    🚑 Add API CRUD for all entities

--- src/Entity/Barber.php
+++ src/Entity/Barber.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BarberRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +11,7 @@ use Ramsey\Uuid\Doctrine\UuidGenerator;
 use Ramsey\Uuid\UuidInterface;
 
 #[ORM\Entity(repositoryClass: BarberRepository::class)]
+#[ApiResource]
 class Barber
 {
     #[ORM\Id]
