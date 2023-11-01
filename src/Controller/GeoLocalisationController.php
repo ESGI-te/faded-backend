6a9933550325163ea15230bf501cb25cd4cb0f4a
tlugat <teo.lugat@ausha.co>
2023-11-01 18:40:27 +0100

    🚑️ Fix establishments search

--- src/Controller/GeoLocalisationController.php
+++ src/Controller/GeoLocalisationController.php
@@ -18,13 +18,10 @@ class GeoLocalisationController
     public function __invoke(
         GeocodingService $geocodingService,
         EstablishmentRepository $establishmentRepository,
-        ServiceRepository $serviceRepository,
-        FeedbackRepository $feedbackRepository,
         #[MapQueryParameter] string $address,
         #[MapQueryParameter] string $categoryId,
-        #[MapQueryParameter] string $rayon,
-    ):JsonResponse
-    {
+        #[MapQueryParameter] string $radius,
+    ): JsonResponse {
 
         $clientCoordinates = $geocodingService->geocodeAddress($address);
         $response = new JsonResponse();
@@ -33,64 +30,39 @@ class GeoLocalisationController
             return $response->setData(['status' => 400, 'data' => "no coordinates"]);
         }
 
-        $establishments = $establishmentRepository->findAllNearToAdress($clientCoordinates['lat'],$clientCoordinates['lng'],$rayon);
+        $establishments = $establishmentRepository->findAllNearToAdress($clientCoordinates['lat'], $clientCoordinates['lng'], $radius);
 
         $sortedEstablishments = [];
 
         foreach ($establishments as $establishment) {
 
-                $categories = $establishment['category'];
-                $uuidArray = explode(',', str_replace(['{', '}'], '', $categories));
-                $uuidArray = array_map('trim', $uuidArray);
+            $categories = $establishment['category'];
+            $uuidArray = explode(',', str_replace(['{', '}'], '', $categories));
+            $uuidArray = array_map('trim', $uuidArray);
 
-                if($categoryId){
-                    foreach ($uuidArray as $cat) {
-                        if ($cat == $categoryId)
-                            $sortedEstablishments[] = [
-                                'establishment_name' => $establishment['name'],
-                                'establishment_adress' => $establishment['address'],
-                                'distance' => number_format($establishment['distance'],2),
-                                'note' => number_format($establishment['moyenne'],2),
-                                'note_count' => $establishment['note_count'],
-                            ];
-                    }}
-                else{
-                    $sortedEstablishments[] = [
-                        'establishment_name' => $establishment['name'],
-                        'establishment_adress' => $establishment['address'],
-                        'distance' => number_format($establishment['distance'],2),
-                        'note' => number_format($establishment['moyenne'],2),
-                        'note_count' => $establishment['note_count'],
-                    ];
+            if ($categoryId) {
+                foreach ($uuidArray as $cat) {
+                    if ($cat == $categoryId)
+                        $sortedEstablishments[] = [
+                            'name' => $establishment['name'],
+                            'address' => $establishment['address'],
+                            'distance' => number_format($establishment['distance'], 2),
+                            'note' => number_format($establishment['moyenne'], 2),
+                            'note_count' => $establishment['note_count'],
+                        ];
                 }
-
+            } else {
+                $sortedEstablishments[] = [
+                    'name' => $establishment['name'],
+                    'address' => $establishment['address'],
+                    'distance' => number_format($establishment['distance'], 2),
+                    'note' => number_format($establishment['moyenne'], 2),
+                    'note_count' => $establishment['note_count'],
+                ];
+            }
         }
 
 
-        return $response->setData(['status' => 200, 'nearest-establishsment' => $sortedEstablishments]);
-    }
-
-
-    #[Route('to-adress', name: 'location-to-adress', methods: ['GET'])]
-    public function geocodeAction(GeocodingService $geocodingService):JsonResponse
-    {
-        $address = 'Tour Eiffel'; // Replace with the address you want to geocode
-        $coordinates = $geocodingService->geocodeAddress($address);
-        $response = new JsonResponse();
-        if ($coordinates) {
-            // Latitude and longitude coordinates
-            $latitude = $coordinates['lat'];
-            $longitude = $coordinates['lng'];
-
-
-            $full_localisation = $latitude . ' ' . $longitude;
-
-            return $response->setData(['status' => 200, $coordinates]);
-
-
-        } else {
-            return $response->setData(['status' => 400, 'data' => "something went wrong"]);
-        }
-
+        return $response->setData($sortedEstablishments);
     }
 }
