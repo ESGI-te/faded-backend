b85998f7140ce9a2af1babaaada99c9cc087d7fa
tlugat <teo.lugat@ausha.co>
2023-11-20 14:21:03 +0100

    Refactor fixtures to use planning array

--- src/DataFixtures/EstablishmentFixtures.php
+++ src/DataFixtures/EstablishmentFixtures.php
@@ -3,9 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Establishment;
-use App\Entity\Feedback;
-use App\Repository\ProviderRepository;
-use App\Repository\WeeklyOpeningHoursRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -15,6 +12,33 @@ class EstablishmentFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public const ESTABLISHEMENT_REFERENCE = 'establishsment_reference';
+    private const PLANNING = [
+        'monday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'tuesday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'wednesday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'thursday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'friday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'saturday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'sunday' => [],
+    ];
 
     public function load(ObjectManager $manager): void
     {
@@ -28,7 +52,7 @@ class EstablishmentFixtures extends Fixture implements DependentFixtureInterface
             $establishment->setAddress($faker->address);
             $establishment->setLatitude($faker->latitude);
             $establishment->setLongitude($faker->longitude);
-            $establishment->setOpeningHours($this->getReference(WeeklyOpeningHoursFixtures::WEEKLY_OPENING_HOURS_REFERENCE . '-' . $i));
+            $establishment->setPlanning(self::PLANNING);
             $establishment->setProvider($this->getReference(ProviderFixtures::PROVIDER_REFERENCE . '-' . $i));
             $establishment->addBarber($this->getReference(BarberFixtures::BARBER_REFERENCE . '-' . $i));
             $this->setReference(self::ESTABLISHEMENT_REFERENCE . '-' . $i, $establishment);
@@ -42,7 +66,6 @@ class EstablishmentFixtures extends Fixture implements DependentFixtureInterface
     {
         return [
             ProviderFixtures::class,
-            WeeklyOpeningHoursFixtures::class,
             BarberFixtures::class,
         ];
     }
