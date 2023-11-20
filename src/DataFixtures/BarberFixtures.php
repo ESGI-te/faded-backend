0c8e386ce8af35e44e6c2acb860b760e2d9194f7
tlugat <teo.lugat@ausha.co>
2023-11-20 14:21:03 +0100

    Refactor fixtures to use planning array

--- src/DataFixtures/BarberFixtures.php
+++ src/DataFixtures/BarberFixtures.php
@@ -3,9 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Barber;
-use App\Entity\Establishment;
-use App\Repository\ProviderRepository;
-use App\Repository\WeeklyOpeningHoursRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -15,28 +12,53 @@ class BarberFixtures extends Fixture implements DependentFixtureInterface
 {
 
     public const BARBER_REFERENCE = 'barber_reference';
+    private const PLANNING = [
+        'monday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'tuesday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'wednesday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'thursday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'friday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'saturday' => [
+            'open' => '2000-01-01 09:00:00',
+            'close' => '2000-01-01 19:00:00',
+        ],
+        'sunday' => [],
+    ];
 
-    public function load(ObjectManager $manager):void
+    public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
         for ($i = 0; $i < 10; $i++) {
             $barber = new Barber();
             $barber->setFirstName($faker->firstName);
             $barber->setLastName($faker->lastName);
-            $barber->setWorkingHours($this->getReference(WeeklyOpeningHoursFixtures::WEEKLY_OPENING_HOURS_BARBER_REFERENCE. '-' . $i));
+            $barber->setPlanning(self::PLANNING);
             $barber->setProvider($this->getReference(ProviderFixtures::PROVIDER_REFERENCE . '-' . $i));
-            $this->addReference(self::BARBER_REFERENCE.'-'.$i,$barber);
+            $this->addReference(self::BARBER_REFERENCE . '-' . $i, $barber);
             $manager->persist($barber);
             $manager->flush();
         }
-
     }
 
     public function getDependencies(): array
     {
         return [
             ProviderFixtures::class,
-            WeeklyOpeningHoursFixtures::class,
         ];
     }
 }
