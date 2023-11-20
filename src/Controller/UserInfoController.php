7a1d725868a342461f44e54b92562a07611d52c7
tlugat <teo.lugat@ausha.co>
2023-11-20 22:24:33 +0100

    ♻️  Refactor entities

--- src/Controller/UserInfoController.php
+++ src/Controller/UserInfoController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Security\Core\User\UserInterface;
+
 #[AsController]
 class UserInfoController extends AbstractController
 {
--- src/DataFixtures/ServiceCategoryFixtures.php
+++ src/DataFixtures/ServiceCategoryFixtures.php
@@ -4,40 +4,26 @@ namespace App\DataFixtures;
 
 use App\Entity\ServiceCategory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Yaml\Yaml;
 
-class ServiceCategoryFixtures extends Fixture implements DependentFixtureInterface
+class ServiceCategoryFixtures extends Fixture
 {
-    public const  CATEGORY_REFERENCE_1 = 'Coupe de cheveux + Coiffage';
-    public const  CATEGORY_REFERENCE_2 = 'Coupe & coiffage + Barbe';
-    public const  CATEGORY_REFERENCE_3 = 'Coloration & Décoloration';
-    public const  CATEGORY_REFERENCE_4 = 'Soins - Visage / Cheveux';
-    public const  CATEGORY_REFERENCE_5 = 'Lissage & Permanente';
-
+    public const  CATEGORY_REFERENCE = 'category_';
 
     public function load(ObjectManager $manager): void
     {
-        $max = 5;
-        for ($i = 0; $i < $max; $i++) {
+        $categories = Yaml::parseFile(__DIR__ . '/data/serviceCategories.yaml');
+        $index = 1;
+
+        foreach ($categories as $categoryData) {
             $category = new ServiceCategory();
-            $referenceName = 'CATEGORY_REFERENCE_' . $i + 1;
-            $dynamicReference = constant("self::$referenceName");
-            $category->setName($dynamicReference);
-            $establishment1 = $this->getReference(EstablishmentFixtures::ESTABLISHEMENT_REFERENCE . '-' . $i);
-            $establishment = $this->getReference(EstablishmentFixtures::ESTABLISHEMENT_REFERENCE . '-' . $max * 2 - 1 - $i);
-            $category->addEstablishment($establishment1);
-            $category->addEstablishment($establishment);
-            $this->addReference($dynamicReference, $category);
+            $category->setName($categoryData['name']);
             $manager->persist($category);
+            $this->addReference(self::CATEGORY_REFERENCE . $index, $category);
+            $index++;
         }
 
         $manager->flush();
-    }
-
-
-    public function getDependencies(): array
-    {
-        return [EstablishmentFixtures::class];
     }
 }
