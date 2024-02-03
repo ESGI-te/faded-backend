c8d83d11f55feb606ca12a608435c8cc74768c23
kachakib <karmimchakib@gmail.com>
2024-02-03 18:05:08 +0100

    ✨ add documentation for stats and use security to get provider

--- src/State/AppointmentCountStatisticsProvider.php
+++ src/State/AppointmentCountStatisticsProvider.php
@@ -4,30 +4,71 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use App\Entity\Provider;
 use App\Repository\AppointmentRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class AppointmentCountStatisticsProvider implements ProviderInterface
 {
 
     private AppointmentRepository $appointmentRepository;
 
-    public function __construct(AppointmentRepository $appointmentRepository)
+    private Security $security;
+
+    public function __construct(AppointmentRepository $appointmentRepository, Security $security)
     {
         $this->appointmentRepository = $appointmentRepository;
+        $this->security = $security;
     }
 
+    /**
+     * @throws \Exception
+     */
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
 
-        $days = (int) $context['filters']['days'];
+        $user = $this->security->getUser();
+        $provider = $user->getProvider();
+        assert($provider instanceof Provider);
+        $id = $provider->getId();
 
-        if($days) {
-            $appointments = $this->appointmentRepository->findByRangAppointments($days);
-            return [
-                'appointments' => $appointments
-            ];
+        $isProvider = $this->security->isGranted('ROLE_PROVIDER');
+        $isBarber = $this->security->isGranted('ROLE_BARBER');
+
+        if(!$isProvider && !$isBarber) {
+            throw new \Exception('You do not have permission for this query');
         }
+
+        $days = (int) $context['filters']['days'];
+        $establishmentId = $context['filters']['establishmentId'] ?? null;
+
+
+
+        if($isProvider && $days && !$establishmentId && !$isBarber) {
+            return $this->getAppointments($id,$days);
+        }else if ($isProvider && $days && $establishmentId && !$isBarber) {
+            return $this->getAppointments($id,$days,$establishmentId);
+        }else if($isBarber && $days) {
+            return $this->getAppointments($id,$days,$establishmentId);
+        }
+
 
         return null;
     }
+
+    private function getAppointments(int $id, int $days, string $establishmentId = null): array
+    {
+        if($establishmentId) {
+            $appointments = $this->appointmentRepository->findByRangAppointments($id,$days,$establishmentId);
+        }
+        else {
+            $appointments = $this->appointmentRepository->findByRangAppointments($id,$days);
+        }
+
+        return [
+            'appointments' => $appointments
+        ];
+    }
 }
+
+
--- src/State/FeedbackProvider.php
+++ src/State/FeedbackProvider.php
@@ -4,28 +4,43 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use App\Entity\Provider;
 use App\Repository\AppointmentRepository;
 use App\Repository\FeedbackRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class FeedbackProvider implements ProviderInterface
 {
     private FeedbackRepository $feedbackRepository;
+    private Security $security;
 
-    public function __construct(FeedbackRepository $feedbackRepository)
+    public function __construct(FeedbackRepository $feedbackRepository, Security $security)
     {
         $this->feedbackRepository = $feedbackRepository;
+        $this->security = $security;
     }
 
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        $providerId = $context['filters']['providerId'];
-        $establishmentId = $context['filters']['establishmentId'];
+        $user = $this->security->getUser();
+        $provider = $user->getProvider();
+        assert($provider instanceof Provider);
+        $id = $provider->getId();
 
-        if($providerId && !$establishmentId) {
-            return $this->feedbackRepository->findAverageFeedbackForProvider($providerId);
+        $isProvider = $this->security->isGranted('ROLE_PROVIDER');
+        $isBarber = $this->security->isGranted('ROLE_BARBER');
+
+        if(!$isProvider && !$isBarber) {
+            throw new \Exception('You do not have permission for this query');
         }
-        elseif($establishmentId && $providerId || !$providerId && $establishmentId){
+
+        $establishmentId = $context['filters']['establishmentId'] ?? null;
+
+        if($isProvider && !$establishmentId) {
+            return $this->feedbackRepository->findAverageFeedbackForProvider($id);
+        }
+        elseif($establishmentId && $isProvider || $isBarber && !$isProvider) {
             return $this->feedbackRepository->findAverageFeedbackForEstablishment($establishmentId);
         }
         else {
