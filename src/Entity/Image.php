b0aed70635b5be774a9e8c324cafc65492e5d239
kachakib <karmimchakib@gmail.com>
2024-02-10 21:26:23 +0100

    ✨ add gallery endpoints with serializers and main cover for establishment

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Post;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ImageRepository;
 use Ramsey\Uuid\Doctrine\UuidGenerator;
@@ -10,20 +13,32 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
+#[ApiResource(
+    operations: [
+        new Post(
+            uriTemplate: '/images',
+            normalizationContext: ['groups' => ['image-read']],
+            denormalizationContext: ['groups' => ['image-write']]
+        ),
+        new Delete()
+    ]
+)]
 class Image
 {
     #[ORM\Id]
     #[ORM\Column(type: "uuid", unique: true)]
     #[ORM\GeneratedValue(strategy: "CUSTOM")]
     #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    #[Groups(['image-read'])]
     private UuidInterface|string $id;
 
-    #[Groups(['establishment-image-read', 'barber-image-read'])]
+    #[Groups(['establishment-image-read', 'barber-image-read','image-read', 'image-write' ])]
     #[ORM\Column(length: 255)]
     #[Assert\Url]
     private ?string $url = null;
 
     #[ORM\ManyToOne(inversedBy: 'images')]
+    #[Groups(['image-read', 'image-write'])]
     private ?Establishment $establishment = null;
 
     public function getId(): ?string
